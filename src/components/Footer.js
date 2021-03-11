1560b2f9687cb91025cf1792b8552fcf324e8d02
kimts2627 <kimtstststs@gmail.com>
2021-03-12 04:52:53 +0900

    Edit Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import logo from '../img/logo-coral.svg';
+import earth from '../img/earth.png';
+import arrow from '../img/arrow.png';
 
 const Footer = () => {
 
@@ -10,14 +12,22 @@ const Footer = () => {
     '회사 소개', '제휴', '추천 강좌', '연락처'
   ]);
 
+  const [currentLanguage, changeLanguage] = useState('한국어');
+
+  const [isLanguageModalOn, handleLanguageModal] = useState(false);
+
   return (
-    <footer className='border-t border-gray-300 h-80 pl-6 pt-8'>
+    <footer className='border-t border-gray-300 h-80 pl-6 pt-8 relative mt-10'>
       <article className='flex flex-wrap'>
-        {footerMenu.map(item => <p className=''>{item}</p>)}
+        {footerMenu.map(item => <p className='w-60 font-normal text-sm mb-2 cursor-pointer'>{item}</p>)}
       </article>
-      <img className='h-10' src={logo} alt='' />
-      <div className='border border-black rounded w-52 h-12'></div>
-      <p>© 2021 Udemy, Inc.</p>
+      <img className='h-8 mt-14' src={logo} alt='' />
+      <div className='border border-black rounded w-52 h-12 absolute top-8 right-8 flex items-center justify-evenly cursor-pointer' onClick={() => handleLanguageModal(!isLanguageModalOn)}>
+        <img className='w-4' src={earth} alt='' />
+        <p className='w-32 flex justify-center items-center'>{currentLanguage}</p>
+        <img className='w-3 rotate-90' src={arrow} alt='' />
+      </div>
+      <p className='text-xs text-gray-500 absolute bottom-14 right-8'>© 2021 Udemy, Inc.</p>
     </footer>
   );
 }
