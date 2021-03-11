a853b6d47b6442583c93d984599eb795dc1407a5
kimts2627 <kimtstststs@gmail.com>
2021-03-12 04:02:17 +0900

    Edit Section-2 style without carousel

--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -1,7 +1,7 @@
 const SectionTwo = () => {
   return (
-    <section className='px-6'>
-      <h1>수강생들이 보고 있는 강좌</h1>
+    <section className='w-full max-w-screen-2xl px-14 mt-14'>
+      <h1 className='font-bold text-2xl'>수강생들이 보고 있는 강좌</h1>
       <Carousel />
     </section>
   );
@@ -10,7 +10,7 @@ const SectionTwo = () => {
 const Carousel = () => {
 
   return(
-    <article className='border border-red-700 h-72 overflow-x-hidden'>
+    <article className='border border-red-700 h-72 overflow-x-hidden mt-5'>
       <div className='w-96'></div>
     </article>
   );
