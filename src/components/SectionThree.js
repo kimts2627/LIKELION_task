bd7663726edf0b2b0eb0b314cfb99fc565e3aa43
kimts2627 <kimtstststs@gmail.com>
2021-03-12 04:06:15 +0900

    Edit Section-3 style

--- src/components/SectionThree.js
+++ src/components/SectionThree.js
@@ -1,9 +1,9 @@
 const SectionThree = () => {
   return (
-    <section className='bg-gray-900 py-10 text-white flex flex-col items-center'>
-      <h1>맞춤형 학습 추천을 받아보세요.</h1>
-      <p>최고의 추천 강좌에 대한 몇 가지 질문에 답해 주세요</p>
-      <button>시작하기</button>
+    <section className='w-full bg-gray-900 py-10 h-56 text-white flex flex-col items-center'>
+      <h1 className='font-medium text-3xl'>맞춤형 학습 추천을 받아보세요.</h1>
+      <p className='mt-3 font-light text-lg'>최고의 추천 강좌에 대한 몇 가지 질문에 답해 주세요</p>
+      <button className='border border-white rounded w-24 h-12 mt-3 focus:outline-none'>시작하기</button>
     </section>
   );
 }
